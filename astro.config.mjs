b5c469ce61c30213068645bf265965ba5b83b0e9
Mikael Brevik <mikaelbre@gmail.com>
2024-04-01 20:50:29 +0200

    fix astro url

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.me',
+  site: 'https://mib.im',
   integrations: [
     expressiveCode(expressiveCodeOptions),
     tailwind({
