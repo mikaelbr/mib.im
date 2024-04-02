e77b10ef145388a2bfef02bec03211ead5dd97dc
Mikael Brevik <mikaelbre@gmail.com>
2024-04-02 19:10:55 +0200

    fix: humanized numbers

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -8,11 +8,16 @@ const cache = new LRUCache<string, RepoStats>({
   ttl: 1000 * 60 * 60,
 });
 
+type HumanizedNumber = {
+  value: number;
+  short: string;
+};
+
 export type RepoStats = {
   name: string;
   url: string;
-  stars: number;
-  forks: number;
+  stars: HumanizedNumber;
+  forks: HumanizedNumber;
   description: string;
 };
 
@@ -66,21 +71,37 @@ export async function getRepoStats(repo: `${string}/${string}`): Promise<RepoSta
     name: data.full_name,
     description: data.description,
     url: data.html_url,
-    stars: data.stargazers_count,
-    forks: data.forks_count,
+    stars: numberToHumanizedNumber(data.stargazers_count),
+    forks: numberToHumanizedNumber(data.forks_count),
   };
 
   cache.set(repo, obj);
   return obj;
 }
 
+function numberToHumanizedNumber(number: number): HumanizedNumber {
+  if (number < 1000) return { value: number, short: number.toString() };
+  if (number < 1000000) return { value: number, short: `${(number / 1000).toFixed(1)}K` };
+  return { value: number, short: `${(number / 1000000).toFixed(1)}M` };
+}
+
 export async function getMultipleRepoStats(repo: `${string}/${string}`[]): Promise<RepoStats[]> {
+  // return [
+  //   {
+  //     name: 'mikaelbr/node-notifier',
+  //     description:
+  //       'A Node.js module for sending notifications on native Mac, Windows, and Linux (or Growl as fallback)',
+  //     url: 'https://test.com',
+  //     stars: numberToHumanizedNumber(3000),
+  //     forks: numberToHumanizedNumber(200),
+  //   },
+  // ];
   const response = await Promise.all(repo.map(getRepoStats));
   return sortProjects(response.filter(Boolean) as RepoStats[]);
 }
 
 export function sortProjects(presentations: RepoStats[]): RepoStats[] {
   return presentations.sort((a, b) => {
-    return b.stars - a.stars;
+    return b.stars.value - a.stars.value;
   });
 }
